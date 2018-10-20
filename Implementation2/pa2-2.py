48766b851311eacd6881a0c05866a417b5b13fdb
rayhunglu <s9509344@gmail.com>
2018-10-20 16:44:00 -0700

    Add files via upload

---
+++ Implementation2/pa2-2.py
@@ -0,0 +1,69 @@
+"""
+@author: raylu
+pa2-2.py
+"""
+
+import numpy as np
+import pandas as pd
+
+class Perceptron:
+
+    # Intial global values
+    def __init__(self, datas, result,vd,vr):
+        self.x = datas                              # training examples
+        self.y = result
+        self.vx=vd
+        self.vy=vr                             
+        self.dataNum = len(datas)
+        self.vdataNum = len(vd)                   # number of data
+    def OnlinePerceptron(self, maxIter=15):
+        # Initail point of weights values
+        self.w = np.ones((len(self.x[0]), 1))      #w=n*1
+        c=0                                    
+        self.averw = np.ones((len(self.x[0]), 1))      #w=n*1
+        s=0                             
+        for i in range(maxIter):
+            tt=0
+            vt=0
+            for j in range(self.dataNum):                 
+                u=np.dot(self.w.T,self.x[j].T)           #(1*n) dot (n*1)
+                yj=float(self.y[j][0]*u[0])
+                if(yj<=0):
+                    if((c+s)>0):
+                        self.averw=((s*self.averw)+(c*self.w))/(s+c)
+                    s=s+c
+                    tem=np.asmatrix(self.x[j])
+                    self.w=self.w+self.y[j][0]*tem.T   
+                    c=0
+                else:
+                    c=c+1
+            for j in range(self.dataNum):                 
+                u=np.dot(self.w.T,self.x[j].T)           #(1*n) dot (n*1)
+                yj=float(self.y[j][0]*u[0])
+                if(yj<=0):
+                    tt+=1
+            for j in range(self.vdataNum):                 
+                u=np.dot(self.w.T,self.vx[j].T)            #(1*n) dot (n*1)
+                vyj=float(self.vy[j][0]*u[0])
+                if(vyj<=0):
+                    vt+=1
+            print(1-(tt/self.dataNum),1-(vt/self.vdataNum))
+        if(c>0):
+            self.averw=((s*self.averw)+(c*self.averw))/(s+c)
+#======
+#main
+#======
+maxIter=15
+df=pd.read_csv("pa2_train.csv",header=None)
+xt=df.iloc[:,1:].values
+yt=df.iloc[:, :1].values
+biast=np.ones((1,len(xt)))
+df=pd.read_csv("pa2_valid.csv",header=None)
+xv=df.iloc[:,1:].values
+yv=df.iloc[:, :1].values
+biasv=np.ones((1,len(xv)))
+xxt=np.append(xt.T,biast,axis=0)
+xxv=np.append(xv.T,biasv,axis=0)
+lg=Perceptron(xxt.T,(yt-4)*(-1),xxv.T,(yv-4)*(-1))
+lg.OnlinePerceptron(maxIter)
+# print(w)
---
+++ Implementation2/pa2-3.py
@@ -0,0 +1,59 @@
+"""
+@author: raylu
+pa2-3.py
+"""
+
+import numpy as np
+import pandas as pd
+
+class Perceptron:
+
+    # Intial global values
+    def __init__(self, datas, result,vd,vr):
+        self.x = datas                              # training examples
+        self.y = result
+        self.vx=vd
+        self.vy=vr                             
+        self.dataNum = len(datas)
+        self.vdataNum = len(vd)                   # number of data
+    def OnlinePerceptron(self, maxIter=15):
+        # Initail point of weights values
+        f=len(self.x[0])
+        self.w = np.zeros((len(self.x[0]), 1))      #w=n*1
+        alpha=np.zeros(self.dataNum,1)
+        for i in range(maxIter):
+            tt=0
+            vt=0
+            for j in range(self.dataNum):                 
+                u=np.dot(self.w.T,self.x[j].T)           #(1*n) dot (n*1)
+                yj=float(self.y[j][0]*u[0])
+                if(yj<=0):
+                    tem=np.asmatrix(self.x[j])
+                    self.w=self.w+self.y[j][0]*tem.T
+            for j in range(self.dataNum):                 
+                u=np.dot(self.w.T,self.x[j].T)           #(1*n) dot (n*1)
+                yj=float(self.y[j][0]*u[0])
+                if(yj<=0):
+                    tt+=1
+            for j in range(self.vdataNum):                 
+                u=np.dot(self.w.T,self.vx[j].T)            #(1*n) dot (n*1)
+                vyj=float(self.vy[j][0]*u[0])
+                if(vyj<=0):
+                    vt+=1
+            print(1-(tt/self.dataNum),1-(vt/self.vdataNum))
+#======
+#main
+#======
+maxIter=15
+df=pd.read_csv("pa2_train.csv",header=None)
+xt=df.iloc[:,1:].values
+yt=df.iloc[:, :1].values
+biast=np.ones((1,len(xt)))
+df=pd.read_csv("pa2_valid.csv",header=None)
+xv=df.iloc[:,1:].values
+yv=df.iloc[:, :1].values
+biasv=np.ones((1,len(xv)))
+xxt=np.append(xt.T,biast,axis=0)
+xxv=np.append(xv.T,biasv,axis=0)
+lg=Perceptron(xxt.T,(yt-4)*(-1),xxv.T,(yv-4)*(-1))
+lg.OnlinePerceptron(maxIter)
