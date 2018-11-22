b1a8843d568bfdd3ea3b6f23ca150dd2ef348586
lu juihung <raylu@mail.hellgate.com>
2018-11-21 19:32:25 -0800

    up

--- Implementation3/pa3-1.py
+++ Implementation3/pa3-1.py
@@ -13,18 +13,6 @@ class Node:
         self.splitfeature = None
         self.splitthreshold=None
 
-# def splitDataSet(datas,feature,sample,threshold):
-#     rightnode=Node()
-#     leftnode=Node()
-#     data=datas[datas[:, feature].argsort()]
-#     leftdatas,rightdatas=np.split(datas,[sample],axis=0)  #split <T,>=T
-#     left=leftdatas[leftdatas[:,0].argsort()]      #split +-1
-#     right=rightdatas[rightdatas[:,0].argsort()]   #split +-1
-#     llnum=np.argmax(left.T[0])          #nums of left of left node
-#     rlnum=np.argmax(right.T[0])
-
-#     return llnum,rlnum,leftdatas,rightdatas
-
 def gini_benefit(datas,sample):
     left,right=np.split(datas,[sample],axis=0)  #split <T,>=T
     if len(left)==0:
@@ -38,57 +26,59 @@ def gini_benefit(datas,sample):
     c=len(datas)
     l=len(left)
     r=len(right)
-    # print(c)
-    # print(l)
-    # print(r)
     # llnum=1
     # rlnum=1
     # c=1
     # l=1
     # r=1
     if l==0 or r==0:
-        return 0,left,right
+        return 0,left,right,llnum,rlnum
     vl=(2*llnum*(l-llnum)/(c*l))
     vr=(2*rlnum*(r-rlnum)/(c*r))
-    return (1-vl-vr),left,right
+    return (1-vl-vr),left,right,llnum,rlnum
 
 def chooseBestFeatureToSplit(datas,root):
     gini=0
     threshold=0
-    y=datas.T[0]
+    # y=datas.T[0]
     leftdatas=None
     rightdatas=None
+    llnum=0
+    rlnum=0
     for i in range(1,len(datas[0])):#feature
-        print(i)
-        value=datas.T[i]
-        data=(np.asarray([y,value])).T
-        data=data[data[:,1].argsort()]
+        # print(i)
+        # value=datas.T[i]
+        # data=(np.asarray([y,value])).T
+        data=datas[datas[:,i].argsort()]
         for j in range(0,len(datas)):#  sample index
-            b,left,right=gini_benefit(data,j)
-            print(b)
+            b,left,right,ll,rl=gini_benefit(data,j)
+            # print(b)
             if b>gini:
                 gini=b
                 feature=i
                 leftdatas=left
                 rightdatas=right
-                threshold=data[j][1]
-    return feature,threshold,leftdatas,rightdatas,gini
+                llnum=ll
+                rlnum=rl
+                threshold=data[j][i]
+    return feature,threshold,leftdatas,rightdatas,gini,llnum,rlnum
 
 def finddecisiontree(datas,root,level,maxdepth): #x=[[],[]]
-    feature,threshold,leftdatas,rightdatas,gini=chooseBestFeatureToSplit(datas,root)
+    feature,threshold,leftdatas,rightdatas,gini,llnum,rlnum=chooseBestFeatureToSplit(datas,root)
     root.splitfeature=feature
     root.splitthreshold=threshold
     rightnode=Node()
     leftnode=Node()
     root.left=leftnode
     root.right=rightnode
-    print('threshold',threshold,feature,'benefit',gini)
+    print(level,':threshold',threshold,feature,'benefit',gini)
     # llnum,rlnum,leftdatas,rightdatas=splitDataSet(datas,feature,sample,threshold)
     if level<maxdepth-1:
         if llnum!=0 and llnum!=len(leftdatas):
             leftnode=finddecisiontree(leftdatas,leftnode,level+1,maxdepth)
         if rlnum!=0 and rlnum!=len(rightdatas):
             rightnode=finddecisiontree(rightdatas,rightnode,level+1,maxdepth)
+    # acc[level]=acc.sefdefault(level,0)+max(llnum,(len(leftdatas)-llnum)+max(rlnum,(len(rightdatas-rlnum))))
     return root
 
 def decisiontree(datas,maxdepth=20):
@@ -100,7 +90,7 @@ def decisiontree(datas,maxdepth=20):
     return node
 
 ####################main#####################
-maxdepth=1
+maxdepth=11
 df=pd.read_csv("pa3_train_reduced.csv",header=None)
 xt=df.iloc[:,:].values
 # yt=df.iloc[:, :1].values
@@ -109,9 +99,10 @@ xv=df.iloc[:,:].values
 # yv=df.iloc[:, :1].values
 datas=xt
 # result=(yt-4)*(-1)
-acc={}
+acc={0:0}
 print (time.asctime( time.localtime(time.time()) ))
 root=decisiontree(datas,maxdepth)
+print(acc)
 print (time.asctime( time.localtime(time.time()) ))
 
 
